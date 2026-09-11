1c7241ca5d55593b1b0e3ff9b7a1c1fbbc138e4f
Edeys <edeys@users.noreply.github.com>
2026-09-11 22:07:06 +0700

    chore: remove one-off helper scripts, debug dumps and local logs

--- _clean.py
+++
@@ -1,7 +0,0 @@
-﻿import re
-with open(r'D:\Dự Án Cá Nhân\xuanloi.me\src\layouts\Layout.astro','r',encoding='utf-8') as f:
-    s = f.read()
-s = s.replace('  : {};\n};', '  : {};')
-with open(r'D:\Dự Án Cá Nhân\xuanloi.me\src\layouts\Layout.astro','w',encoding='utf-8') as f:
-    f.write(s)
-print('cleaned')
